53e23bc7d62e8e176c676354328ca232c7ca9dfe
Teddy Byron <ted@tedbyron.com>
2022-01-22 03:04:50 -0500

    move env reads to proper places

--- src/handler.rs
+++ src/handler.rs
@@ -1,23 +1,84 @@
 //! Event handlers.
 
+use std::env;
+
 use serenity::async_trait;
 use serenity::client::{Context, EventHandler};
 use serenity::model::channel::Message;
-use serenity::model::prelude::Ready;
+use serenity::model::oauth2::OAuth2Scope;
+use serenity::model::prelude::{Activity, Ready};
+use serenity::model::Permissions;
 
 pub struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "pong!").await {
+        if msg.content == "order up" {
+            if let Err(e) = msg
+                .channel_id
+                .say(&ctx.http, "<:galleyboy:915674675684712509>")
+                .await
+            {
                 tracing::error!("{:?}", e);
             }
         }
     }
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         tracing::debug!(guilds = ?ready.guilds);
         tracing::info!("Bot logged in as {}", ready.user.tag());
+
+        // Bot user activity.
+        let activity = match (
+            env::var("TEDBOT_ACTIVITY_TYPE"),
+            env::var("TEDBOT_ACTIVITY_NAME"),
+        ) {
+            (Ok(type_), Ok(name)) => {
+                let activity_type = if let type_ @ ("competing" | "listening" | "playing"
+                | "streaming" | "watching") = type_.as_str()
+                {
+                    type_
+                } else {
+                    tracing::warn!("Invalid TEDBOT_ACTIVITY_TYPE env var");
+                    ""
+                };
+
+                match activity_type {
+                    "competing" => Some(Activity::competing(name)),
+                    "listening" => Some(Activity::listening(name)),
+                    "playing" => Some(Activity::playing(name)),
+                    "streaming" => {
+                        if let Ok(streaming) = env::var("TEDBOT_ACTIVITY_STREAMING") {
+                            Some(Activity::streaming(name, streaming))
+                        } else {
+                            tracing::warn!("Missing TEDBOT_ACTIVITY_STREAMING env var");
+                            None
+                        }
+                    }
+                    "watching" => Some(Activity::watching(name)),
+                    _ => None,
+                }
+            }
+            _ => None,
+        };
+
+        if let Some(activity) = activity {
+            ctx.set_activity(activity).await;
+        }
+
+        let permissions = Permissions::READ_MESSAGES
+            | Permissions::READ_MESSAGE_HISTORY
+            | Permissions::SEND_MESSAGES;
+        let scopes = &[OAuth2Scope::Bot, OAuth2Scope::ApplicationsCommands];
+
+        if let Ok(url) = ready
+            .user
+            .invite_url_with_oauth2_scopes(ctx.http, permissions, scopes)
+            .await
+        {
+            tracing::info!("{}", url);
+        } else {
+            tracing::warn!("Could not create a bot invite URL");
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@
 #![windows_subsystem = "console"]
 #![doc = include_str!("../README.md")]
 
-mod config;
 mod handler;
 
-use std::{env, process};
+use std::env;
+use std::process;
 
 use serenity::Client;
 use tracing_subscriber::EnvFilter;
@@ -44,13 +44,52 @@ async fn run() -> crate::Result<()> {
         .with_env_filter(EnvFilter::from_env("TEDBOT_LOG"))
         .init();
 
-    let cfg = config::load()?;
-
     // NOTE: Intersperse is not yet stable https://github.com/rust-lang/rust/issues/79524
     tracing::trace!(command = %env::args().collect::<Vec<_>>().join(" "));
 
-    let mut client = Client::builder(&cfg.token)
+    // Config from env vars.
+    let token = match env::var("TEDBOT_TOKEN") {
+        Ok(token) => token,
+        Err(_) => return Err(Box::from("Missing TEDBOT_TOKEN env var")),
+    };
+    let app_id = match env::var("TEDBOT_APPLICATION_ID") {
+        Ok(app_id) => match app_id.parse::<u64>() {
+            Ok(app_id_int) => app_id_int,
+            Err(_) => return Err(Box::from("Invalid TEDBOT_APPLICATION_ID env var")),
+        },
+        Err(_) => return Err(Box::from("Missing TEDBOT_APPLICATION_ID env var")),
+    };
+    // let token_components = match client::parse_token(&token) {
+    //     Some(components) => components,
+    //     None => return Err(Box::from("Invalid TEDBOT_TOKEN env var")),
+    // };
+
+    // Guild whitelist.
+    // let whitelist = match env::var("TEDBOT_WHITELIST") {
+    //     Ok(wl_string) => {
+    //         if wl_string.is_empty() {
+    //             None
+    //         } else {
+    //             let wl = wl_string
+    //                 .trim()
+    //                 .split(|c: char| !c.is_ascii_digit())
+    //                 .map(str::parse::<u64>)
+    //                 .filter_map(Result::ok)
+    //                 .map(GuildId::from)
+    //                 .collect::<Vec<_>>();
+
+    //             if wl.is_empty() {
+    //                 tracing::warn!("Invalid TEDBOT_WHITELIST env var, ignoring");
+    //             }
+    //             Some(wl)
+    //         }
+    //     }
+    //     Err(_) => None,
+    // };
+
+    let mut client = Client::builder(token)
         .event_handler(handler::Handler)
+        .application_id(app_id)
         .await?;
     client.start_autosharded().await?;
 
